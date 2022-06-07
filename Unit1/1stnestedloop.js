7ca092792c3837edff25ed374444459ed942bb26
tarunaNangla <tarunanangla131@gmail.com>
2022-06-07 14:32:24 +0530

    afterclass

---
+++ Unit1/1stnestedloop.js
@@ -0,0 +1,18 @@
+
+
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+
+
+ for(var i=1;i<=5;i++)
+ {
+     var bag="";
+     for(var  j=1;j<=5;j++)
+     {
+         bag=bag+j
+     }
+     console.log(bag)
+ }
---
+++ Unit1/1stpattern.js
@@ -0,0 +1,16 @@
+
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+
+for(var i=1;i<=4;i++)
+{
+    var  bag=""
+    for(var j=1;j<=4;j++)
+    {
+        bag=bag+"*"+" ";
+    }
+    console.log(bag)
+}
